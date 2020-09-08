1b8903be86f62b6c5edaabf94a6b2de762f23ce5
Julyo <julyo110701@gmail.com>
2020-09-08 23:09:07 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+// Navbar Scroll
+$('.page-scroll').on('click', function (e) {
+    var href = $(this).attr('href');
+    var elemenHref = $(href);
+
+    $('html,body').animate({
+        scrollTop: elemenHref.offset().top - 50
+    }, 750);
+
+    e.preventDefault();
+});
+
+// Parallax
+$(window).scroll(function () {
+    // Jumbotron
+    var wScroll = $(this).scrollTop();
+
+    $('.jumbotron img').css({
+        'transform': 'translate(0px, ' + wScroll / 4 + '% )'
+    });
+
+    $('.jumbotron h1').css({
+        'transform': 'translate(0px, ' + wScroll / 2 + '% )'
+    });
+
+    $('.jumbotron p').css({
+        'transform': 'translate(0px, ' + wScroll / 1 + '% )'
+    });
+
+    // About
+    if (wScroll > $('.about').offset().top - 330) {
+        $('.about .gambarAbout').each(function (i) {
+            setTimeout(function () {
+                $('.about .gambarAbout').eq(i).addClass('muncul');
+            }, 300 * i)
+        });
+    }
+
+    if (wScroll > $('.about').offset().top - 330) {
+        $('.about .pAbout').each(function (i) {
+            setTimeout(function () {
+                $('.about .pAbout').eq(i).addClass('muncul');
+            }, 300 * i)
+        });
+    }
+
+    // Portfolio
+    if (wScroll > $('.portfolio').offset().top - 330) {
+        $('.portfolio .gambarPorto').each(function (i) {
+            setTimeout(function () {
+                $('.portfolio .gambarPorto').eq(i).addClass('show');
+            }, 300 * i)
+        });
+    }
+
+    if (wScroll > $('.portfolio').offset().top - 280) {
+        $('.portfolio .pPorto').each(function (i) {
+            setTimeout(function () {
+                $('.portfolio .pPorto').eq(i).addClass('show');
+            }, 300 * i)
+        });
+    }
+});
